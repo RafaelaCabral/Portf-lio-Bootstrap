ca3c21b53ef234801b0d831b8e3e580fc2b53892
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2025-04-10 21:19:00 -0300

    fix:ajuste no carousel, validação e máscaras operativas

--- js/main.js
+++ js/main.js
@@ -1,54 +1,49 @@
 $(document).ready(function () {
     $('#carousel-imagens').slick({
-        autoplay: true
+        autoplay: true,
+        arrows: false
     });
-    $('.menu-hamburguer').click(function () {
-        $('nav').slideToggle();
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
     })
 
-    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    })
 
     $('form').validate({
         rules: {
             nome: {
-                required: true,
+                required: true
             },
             email: {
                 required: true,
                 email: true
             },
             telefone: {
-                required: true,
+                required: true
             },
-            mensagem: {
-                required: true,
+            endereco: {
+                required: true
             },
-            veiculoDeInteresse: {
-                required: false,
-            }
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
         },
-        messages: {
-            nome: 'Por favor, insira o seu nome'
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
         },
-        submitHandler: function(form){
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador){
-            let camposIncorretos = validador.numberOfInvalids();
-            if(camposIncorretos){
-                alert(`Existem ${camposIncorretos} campos vazios`)
-            }
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
         }
     })
 
-    $('.lista-veiculos button').click(function(){
-        const destino = $('#contato');
-
-        const nomeVeiculo = $(this).parent().find('h3').text();
-
-        $('#veiculo-interesse').val(nomeVeiculo)
-        $('html').animate({
-            scrollTop: destino.offset().top
-        }, 1000)
-    })
 })
